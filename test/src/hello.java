e3d6cdf0a1e32d99ca5ada23693d3554b61bb950
lzns960 <lzns960@gmail.com>
2021-02-19 21:43:44 +0900

    push

--- test/src/hello.java
+++ test/src/hello.java
@@ -1,5 +1,5 @@
 public class hello {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World himdleda");
     }
 }
